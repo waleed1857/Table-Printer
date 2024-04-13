af8c9212e4d1682112bb3673d44ee8bdaaec20c6
waleed1857 <163301387+waleed1857@users.noreply.github.com>
2024-04-13 19:35:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ var btn = document.getElementById("myOutput");
 // Adding Event to display table:
 btn.addEventListener("click",function(){
 // Grapping inside the function to display correctly
-    var inputElement = document.getElementById("myInput");
-    var inputValue = inputElement.value;
-    alert("This script display table!");
+    let Output = document.getElementById("disOutput");
+    let inputElement = document.getElementById("myInput");
+    let inputValue = inputElement.value;
+    alert("This script will display table!");
         for (let i=1 ; i<=10 ; i++)
         {
+            Output.innerHTML += `
+            <div>${inputValue + " x " + i + " = " + inputValue*i}</div>`
             console.log(inputValue + " x " + i + " = " + inputValue*i);
-            document.write(inputValue + " x " + i + " = " + inputValue*i);
-            document.write("<br>")
         }
     });
